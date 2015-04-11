50818c4280b0da3f26f2e9b732f7618ccecaa98e
thibaud <thibaud@Thibaud-PC>
2015-04-11 18:55:05 +0200

    ajout d'une exception

---
+++ org.eclipse.papyrus.diagramdrawer/src/org/eclipse/papyrus/diagramdrawer/exceptions/TargetOrSourceNotDrawnException.java
@@ -0,0 +1,13 @@
+package org.eclipse.papyrus.diagramdrawer.exceptions;
+
+
+/**
+ * 
+ * @author Thibaud VERBAERE
+ *
+ */
+public class TargetOrSourceNotDrawnException extends DrawerException {
+
+	private static final long serialVersionUID = 5983301756864560102L;
+
+}
