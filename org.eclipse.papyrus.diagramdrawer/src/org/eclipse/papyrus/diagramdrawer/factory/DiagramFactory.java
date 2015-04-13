679800985c4bcd93fb8775dc4158d7b24ce744db
lan21 <allan.rakotoarivony@gmail.com>
2015-04-13 16:43:39 +0200

    il reste le diagram factory

--- org.eclipse.papyrus.diagramdrawer/src/org/eclipse/papyrus/diagramdrawer/factory/DiagramFactory.java
+++ org.eclipse.papyrus.diagramdrawer/src/org/eclipse/papyrus/diagramdrawer/factory/DiagramFactory.java
@@ -1,5 +1,12 @@
 package org.eclipse.papyrus.diagramdrawer.factory;
 
+/**
+ * Lets to create new papyrus diagram inside a project programatically. The main use is to call
+ * the method build to create a new diagram
+ * 
+ * @author Allan Rakotoarivony
+ *
+ */
 public class DiagramFactory {
 
 }
--- org.eclipse.papyrus.diagramdrawer/src/org/eclipse/papyrus/diagramdrawer/factory/ProjectFactory.java
+++ org.eclipse.papyrus.diagramdrawer/src/org/eclipse/papyrus/diagramdrawer/factory/ProjectFactory.java
@@ -1,5 +1,27 @@
 package org.eclipse.papyrus.diagramdrawer.factory;
 
-public class ProjectFactory {
+import org.eclipse.papyrus.diagram.programmaticcreation.othersources.EclipseProject;
+import org.eclipse.papyrus.diagram.programmaticcreation.othersources.ExecutionException;
 
+/**
+ * Lets to create new papyrus project programatically.
+ * The main use is to call the method build to create a new project
+ * @author Allan Rakotoarivony
+ *
+ */
+public class ProjectFactory {
+	
+	public final static ProjectFactory instance= new ProjectFactory();
+	
+	private ProjectFactory(){};
+	
+	/**
+	 * Creates a new eclipse project with the given project name or loads it if it exists in the workspace
+	 * @param projectName
+	 * @return
+	 * @throws ExecutionException
+	 */
+	public EclipseProject build(String projectName) throws ExecutionException{
+		return new EclipseProject(projectName);
+	}
 }
