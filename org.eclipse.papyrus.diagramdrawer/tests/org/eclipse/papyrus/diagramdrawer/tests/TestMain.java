e526be83d9ed75501a083028fc86ef7e51ffd8ea
thibaud <thibaud@Thibaud-PC>
2015-04-28 19:14:26 +0200

    retouche

--- org.eclipse.papyrus.diagramdrawer/tests/org/eclipse/papyrus/diagramdrawer/tests/TestMain.java
+++ org.eclipse.papyrus.diagramdrawer/tests/org/eclipse/papyrus/diagramdrawer/tests/TestMain.java
@@ -4,16 +4,14 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.papyrus.diagramdrawer.factories.ProjectFactory;
 import org.eclipse.papyrus.diagramdrawer.utils.EclipseProject;
 import org.eclipse.papyrus.diagramdrawer.utils.ExecutionException;
 
 public class TestMain {
 
 	public static void main(String[] args) throws ExecutionException, CoreException {
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IProject project = root.getProject("my main project");
-		project.create(null);
-		project.open(null);
+		ProjectFactory.instance.build("blabla");
 	}
 
 }
